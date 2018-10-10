d00dc9bbbd5bb8a48bffb8256eaa6ce304faa867
Doubleday <kdoubled@nrel.gov>
2018-10-09 20:48:49 -0600

    Bug fix time series plot time alignment + aesthetic changes

--- R/ts_forecast.R
+++ R/ts_forecast.R
@@ -106,11 +106,11 @@ plot.ts_forecast <- function(x, ..., actuals=NA) {
       plotdata[,i] <- unname(x$forecasts[[i]]$quantiles)
     } else plotdata[,i] <- 0
   }
-  graphics::plot(NULL, xlim=c(0, length(x)*x$time_step), ylim=c(0, max(plotdata)), xlab="Time", ylab="Aggregate power [W]")
+  graphics::plot(NULL, xlim=c(0, length(x)*x$time_step), ylim=c(0, max(plotdata)), xlab="Time [Hrs]", ylab="Aggregate power [W]")
   fanplot::fan(plotdata, data.type='values', probs=probs, fan.col=colorspace::sequential_hcl,
-               rlab=NULL, start=1, frequency=1/x$time_step)
+               rlab=NULL, start=x$time_step, frequency=1/x$time_step)
   if (all(!is.na(actuals))) {
-    graphics::lines(seq_along(x), actuals, col='gray95', lwd=2)
+    graphics::lines(seq(from=x$time_step, length.out=length(x), by=x$time_step), actuals, col='chocolate3', lwd=2)
   }
 }
 
