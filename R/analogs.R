072ebf75e3ec5b3fca843c2524caa8bec1c8146b
Doubleday <kdoubled@nrel.gov>
2019-05-23 16:37:28 -0600

    Remove feature argument from analog lookup and allow matching near sunrise/set
    --Feature distance assigned 0 if both historical and current values are NA

--- R/analogs.R
+++ R/analogs.R
@@ -5,13 +5,12 @@
 #' @param h_train matrix of historical forecast data, rows for time-steps, columns for physical variables
 #' @param h_real Historical realized value of interest, e.g., power (equivalent to a kNN classification)
 #' @param n Integer, number of historical analogs to pick
-#' @param features Vector of the column names to use for similarity measurement
 #' @param weights Vector of weights to use for each feature
 #' @return A matrix of the analogs, one analog per row, columns for physical variables + realized value
-get_historical_analogs <- function(f_test, h_train, h_real, n, features, weights) {
-  # Error check arguments
+get_historical_analogs <- function(f_test, h_train, h_real, n, weights) {
+    # Error check arguments
   if (dim(f_test)[1] %% 2 == 0) stop('Forecast data must have an odd number of time-steps for centered analog matching.')
-  if (length(features) != length(weights)) stop(paste('Given', length(features), 'features and', length(weights), 'weights. Must be same length.', sep=' '))
+  if (dim(f_test)[2] != length(weights)) stop("Must have same number of weights as physical features")
   if (sum(weights) != 1) stop('Weights must sum to 1.')
   if (n < 1) stop(paste('At least 1 analog required. Requested', n, 'analogs.', sep=' '))
 
@@ -19,7 +18,7 @@ get_historical_analogs <- function(f_test, h_train, h_real, n, features, weights
   sigmas <- apply(h_train, 2, sd, na.rm=T)
 
   metrics <- vapply(1:dim(h_train)[1], delle_monache_distance, numeric(1),
-                     f=f_test, h=h_train, features=features, weights=weights, sigmas=sigmas)
+                     f=f_test, h=h_train, weights=weights, sigmas=sigmas)
 
   # NA's get removed
   indices <- order(metrics, na.last=NA)
@@ -38,30 +37,36 @@ get_historical_analogs <- function(f_test, h_train, h_real, n, features, weights
 #'
 #' @param t_prime Centered time index of the potential analog
 #' @param f forecast of features the over the time interval of interest
-#' @param h historical forecasts of the features
-#' @param features Vector of the column names to use for similarity measurement
+#' @param h historical forecasts of features
 #' @param weights Vector of weights to use for each feature
 #' @param sigmas Standard deviations of the features over the historical record
 #' @return Delle Monache distance
-delle_monache_distance <- function(t_prime, f, h, features, weights, sigmas) {
+delle_monache_distance <- function(t_prime, f, h, weights, sigmas) {
   half_interval <- (dim(f)[1] - 1)/2
 
   if (t_prime <= half_interval | t_prime >= dim(h)[1]-half_interval+1) {
     return(NA)
   } else {
-   feat_dist <- mapply(feature_distance, features, weights, sigmas, MoreArgs = list(f=f, h=h[(t_prime-half_interval):(t_prime+half_interval), features]))
+   feat_dist <- mapply(feature_distance, seq_len(dim(f)[2]), weights, sigmas, MoreArgs = list(f=f, h=h[(t_prime-half_interval):(t_prime+half_interval),]))
    return(sum(feat_dist))
   }
 }
 
 #' Calculate the distance metric for one physical feature for a potential analog
+#' Raw distance is 0 if BOTH forecast and historical are missing
+#' to accommodate windows that overlap sunrise/set. Distance is still NA if only one is missing.
 #'
-#' @param feature a column name
-#' @param weight weight [0,1] assigned to that feature
+#' @param i index of feature column
+#' @param weight weight [0,1] assigned to this feature
 #' @param sigma Standard deviation of the feature over the historical record
 #' @param f forecast of features the over the time interval of interest
 #' @param h possible historical analog of the features, sliced to equivalent time interval
 #' @return feature distance
-feature_distance <- function(feature, weight, sigma, f, h) {
- return(weight/sigma*sqrt(sum((f[,feature]-h[,feature])^2)))
+feature_distance <- function(i, weight, sigma, f, h) {
+  diff <- f[,i]-h[,i]
+  # Overwrite with distance of 0 if both the forecast and historical were NA -- for times overlapping with sunrise/set
+  diff[is.na(f[,i]) & is.na(h[,i])] <- 0
+ return(weight/sigma*sqrt(sum((diff)^2)))
 }
+
+
--- tests/testthat/test_analogs.R
+++ tests/testthat/test_analogs.R
@@ -2,30 +2,31 @@ context("Test forecasting context")
 
 library(forecasting)
 
-fake_data <- matrix(c(1, 2, 1, 10, 20, 10, 100, 200, 100), nrow=3, ncol=3)
-colnames(fake_data) <- c('irr', 'press', 'precip')
+fake_data <- matrix(c(1, 2, 1, 10, 20, 10), nrow=3, ncol=2)
+colnames(fake_data) <- c('irr', 'press')
 
 weights <- c(0.8, 0.2)
-h_data <- matrix(c(1, 2.2, 1, 2, 1.1, 11, 20, 10, 22, 10, 110, 220, 110, 180, 110), nrow=5, ncol=3)
-colnames(h_data) <- c('irr', 'press', 'precip')
+h_data <- matrix(c(1, 2.2, 1, 2, 1.1, 11, 20, 10, 22, 10), nrow=5, ncol=2)
+colnames(h_data) <- c('irr', 'press')
 h_real <- c(1.5, 1, 5, 2, 0)
 an1_irr <- weights[1]/sd(h_data[,'irr'])*0.2
 an1_press <- weights[2]/sd(h_data[,'press'])*1
 an1 <- an1_irr + an1_press
 an2 <- weights[1]/sd(h_data[,'irr'])*0.1 + weights[2]/sd(h_data[,'press'])*2
-features <- c('irr', 'press')
+fake_distance <- c(NA, 0.2, 0.3, 0.1, NA)
 
 test_that("Get analogs throws errors. ", {
-  expect_error(get_historical_analogs(fake_data, NULL, NULL, 1, c('feat1', 'feat2'), c(0.1))) # Unequal weights and features
-  expect_error(get_historical_analogs(fake_data[1:2,], NULL, NULL, 1, c('feat1', 'feat2'), weights)) # Even number of time points
-  expect_error(get_historical_analogs(fake_data, NULL, NULL, 1, c('feat1', 'feat2'), c(0.7, 0.2))) # Weights don't add to 1
-  expect_error(get_historical_analogs(fake_data, NULL, NULL, 0, c('feat1', 'feat2'), weights)) # Fewer than 1 analogs
+  expect_error(get_historical_analogs(fake_data, NULL, NULL, 1, c(0.1))) # Unequal weights and features
+  expect_error(get_historical_analogs(fake_data[1:2,], NULL, NULL, 1,weights)) # Even number of time points
+  expect_error(get_historical_analogs(fake_data, NULL, NULL, 1, c(0.7, 0.2))) # Weights don't add to 1
+  expect_error(get_historical_analogs(fake_data, NULL, NULL, 0, weights)) # Fewer than 1 analogs
 })
 
 test_that("Analog selection is correct.", {
-  out <- get_historical_analogs(fake_data, h_data, h_real, 2, features, weights)
+  with_mock(delle_monache_distance=function(t, ...) return(fake_distance[t]),
+            out <- get_historical_analogs(fake_data, h_data, h_real, 2, weights))
   expect_equal(out$real, c(2, 1))
-  expect_equal(out$distance, c(an2, an1))
+  expect_equal(out$distance, c(0.1, 0.2))
   expect_equal(out$forecast, h_data[c(4, 2),])
 })
 
@@ -37,12 +38,34 @@ test_that("Analog selection handles NA's in metrics.", {
 })
 
 test_that("Delle Monache feature distance calculation is correct", {
-  expect_equal(feature_distance('irr', weights[1], sd(h_data[,'irr']), fake_data, h_data[1:3,]), an1_irr)
+  expect_equal(feature_distance(1, weights[1], sd(h_data[,1]), fake_data, h_data[1:3,]), an1_irr)
+})
+
+test_that("Delle Monache feature distance is 0 for matching NAs", {
+  h_data <- matrix(c(NA, NA, 1, 2, 1.1, 11, 20, 10, 22, 10), nrow=5, ncol=2)
+  fake_data <- matrix(c(NA, NA, 1, 10, 20, 10), nrow=3, ncol=2)
+  expect_equal(feature_distance(1, weights[1], sd(h_data[,1], na.rm=T), fake_data, h_data[1:3,]), 0)
+})
+
+test_that("Delle Monache feature distance is NA for non-matching NAs", {
+  h_data <- matrix(c(NA, NA, 1, 2, 1.1, 11, 20, 10, 22, 10), nrow=5, ncol=2)
+  fake_data <- matrix(c(NA, 2.2, 1, 10, 20, 10), nrow=3, ncol=2)
+  expect_true(is.na(feature_distance(1, weights[1], sd(h_data[,1], na.rm=T), fake_data, h_data[1:3,])))
 })
 
 test_that("Delle Monache total distance calculation is correct", {
-  expect_equal(delle_monache_distance(2, fake_data, h_data,  features, weights, c(sd(h_data[,'irr']), sd(h_data[,'press']))), an1)
-  expect_equal(delle_monache_distance(4, fake_data, h_data,  features, weights, c(sd(h_data[,'irr']), sd(h_data[,'press']))), an2)
-  expect_true(is.na(delle_monache_distance(1, fake_data, h_data,  features, weights, c(sd(h_data[,'irr']), sd(h_data[,'press'])))))
-  expect_true(is.na(delle_monache_distance(5, fake_data, h_data,  features, weights, c(sd(h_data[,'irr']), sd(h_data[,'press'])))))
+  expect_equal(delle_monache_distance(2, fake_data, h_data, weights, c(sd(h_data[,'irr']), sd(h_data[,'press']))), an1)
 })
+
+test_that("Delle Monache total distance is missing for edge cases", {
+  expect_true(is.na(delle_monache_distance(1, fake_data, h_data, weights, c(sd(h_data[,'irr']), sd(h_data[,'press'])))))
+  expect_true(is.na(delle_monache_distance(5, fake_data, h_data, weights, c(sd(h_data[,'irr']), sd(h_data[,'press'])))))
+})
+
+test_that("Delle Monache total distance is NA if a feature is NA", {
+  h_data <- matrix(c(NA, NA, 1, 2, 1.1, 11, 20, 10, 22, 10), nrow=5, ncol=2)
+  fake_data <- matrix(c(NA, 2.2, 1, 10, 20, 10), nrow=3, ncol=2)
+  sigmas <- apply(h_data, 2, sd, na.rm=T)
+  expect_true(is.na(delle_monache_distance(2, fake_data, h_data, weights, sigmas)))
+})
+
