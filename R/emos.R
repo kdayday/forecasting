9ace721e52cbc09640b2f1c2af58d74480d0b9e3
Doubleday <kdoubled@nrel.gov>
2019-12-02 11:14:43 -0700

    Bug fix EMOS to square parameters to control non-negativity

--- R/emos.R
+++ R/emos.R
@@ -1,4 +1,5 @@
 #' Fit EMOS coefficients a, b_k, c, d for truncated normal
+#' b_k, c, and d are parameterized as squares to control non-negativity of regression and scale parameters, like the "square" coefRule and varRule in the ensembleMOS package.
 #' @param tel Vector of training telemetry data
 #' @param ens Matrix of training ensemble member data [time x member]
 #' @param par_init Initial parameter values as a list with a, b, c, d
@@ -12,13 +13,17 @@ emos_model <- function(tel, ens, max_power, par_init=NA) {
     # Start off with a simple linear model
     start_coefs <- unname(lm(tel~ens)$coef)
     # C, D, A, B
-    par <- c(5, 1, start_coefs)
-  } else {par <- c(par_init$c, par_init$d, par_init$a, par_init$b)}
+    par_init <- list(a=start_coefs[1], b=start_coefs[-1], c=5, d=1)
+  }
+
+  # Square-root b, c, d parameters
+  par <- c(sqrt(par_init$c), sqrt(par_init$d), par_init$a, sqrt(abs(par_init$b)))
 
   lb <- c(0, 0, rep(-Inf, times=length(par)-2)) # Force variance parameters to be lower bounded at 0
   opt <- optim(par, tnorm_crps_obj, obs=tel, ens=ens, max_power=max_power, method="L-BFGS-B", lower=lb, control=list(maxit=1e7))
 
-  return(list(a=opt$par[3], b=opt$par[-(1:3)], c=opt$par[1], d=opt$par[2]))
+  # Re-square b, c, d parameters
+  return(list(a=opt$par[3], b=opt$par[-(1:3)]^2, c=opt$par[1]^2, d=opt$par[2]^2))
 }
 
 
@@ -28,10 +33,11 @@ emos_model <- function(tel, ens, max_power, par_init=NA) {
 #' @param ens a [time x member] (i.e., [m x n]) matrix of the ensemble forecasts for each observation
 #' @param max_power Site AC power, for upper limit of truncated normal
 tnorm_crps_obj <- function(par, obs, ens, max_power) {
-  C <- par[1]
-  D <- par[2]
+  # Re-square parameters
+  C <- par[1]^2
+  D <- par[2]^2
   A <- par[3]
-  B <- par[-(1:3)]
+  B <- par[-(1:3)]^2
 
   if (length(B) != ncol(ens)) stop("Mismatch between number of parameters and number of ensemble members.")
 
@@ -40,7 +46,6 @@ tnorm_crps_obj <- function(par, obs, ens, max_power) {
   std_dev <- C + D*S2
   crps <- mean(scoringRules::crps_tnorm(obs, location = mn, scale = std_dev, lower=0, upper=max_power), na.rm=T)
 
-  if (!is.finite(crps)) browser()
   return(crps)
 }
 
--- tests/testthat/test_emos.R
+++ tests/testthat/test_emos.R
@@ -15,18 +15,18 @@ test_that('emos_model calculation is correct for default coefficients', {
   expect_equal(OUT$d, 1)
 })
 
-test_that('emos_model calculation is correct for given coefficients', {
+test_that('emos_model calculation is correct for given coefficients, including negative b values', {
   with_mock(optim = function(par, ...) return(list(par=par)),
-            OUT <- emos_model(tel=c(10, 11), ens=matrix(c(2, 3, 5, 7), ncol=2), max_power=10, par_init=list(a=1, b=c(2, 3), c=4, d=5)))
+            OUT <- emos_model(tel=c(10, 11), ens=matrix(c(2, 3, 5, 7), ncol=2), max_power=10, par_init=list(a=1, b=-c(2, 3), c=4, d=5)))
   expect_equal(OUT$a, 1)
   expect_equal(OUT$b, c(2, 3))
   expect_equal(OUT$c, 4)
   expect_equal(OUT$d, 5)
 })
 
-test_that('tnorm_crps_obj calculation is correct', {
+test_that('tnorm_crps_obj calculation is correct, including parameter squaring', {
   ens <- matrix(1:4, ncol=2)
-  par <- c(0, 2, 1, 2, 3) # a=1, b=c(2,3) c = 0, d = 2
+  par <- c(0, sqrt(2), 1, sqrt(2), sqrt(3)) # a=1, b=c(2,3) c = 0, d = 2
   max_power <- 10
   obs <- c(17, 18)
   # location = 12, 17; s2 = 3, 8; std_dev = 6, 16
